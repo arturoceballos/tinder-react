3b2fdc0592bdc5622cc25641f4fc679a583be8c8
arturoceballos <aceballos118@gmail.com>
2016-01-18 23:54:07 -0800

    Deleted all previous work to start from scratch

--- src/components/title.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Title = () => {
-    return (
-        <h1>TubeForYou!</h1>
-    );
-};
-
-export default Title;
--- src/index.js
+++ src/index.js
@@ -3,45 +3,13 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search';
 
-import SearchBar from './components/search_bar';
-import VideoList from './components/video_list';
-import VideoDetail from './components/video_detail';
-import Title from './components/title';
-
-const API_KEY = 'AIzaSyCR6E9yQT_yXvf6pmTSFKg32T687Er48XM';
-
 class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            videos: [],
-            selectedVideo: null
-        };
-
-        this.videoSearch('snowboards');
-    }
-
-    videoSearch(term) {
-        YTSearch({key: API_KEY, term: term}, (videos) => {
-            this.setState({
-                videos: videos,
-                selectedVideo: videos[0]
-            });
-        });
-    }
 
     render() {
-        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300 );
 
         return (
             <div>
-                <Title />
-                <SearchBar onSearchTermChange={videoSearch} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList
-                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-                    videos={this.state.videos} />
+                Yo
             </div>
         );
     }
